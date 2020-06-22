d2e30528aab47791a726846fd5131364bc066917
dldimash <67282332+dldimash@users.noreply.github.com>
2020-06-22 11:12:38 -0500

    Add files via upload

---
+++ Dimas.R
@@ -0,0 +1,29 @@
+#Respuesta, hay que configurar la entrada de x como una matrix. 
+
+makeCacheMatrix<-function(x=matrix()){
+  inv < -NULL
+  set<-function(y){
+    x<<-y
+    inv <<-NULL
+  }
+  get<-function() x
+  setInverse <-function(solveMatrix)  inv << - solveMAtrix
+  getInverse <-function () inv
+  list(set=set, get= get,
+       setInverse=setInverse,
+       getInverse= getInverse)
+}
+
+#respuesta a la CacheSolve
+
+cacheSolve<-function(x, ...){
+  inv<-x$getInverse()
+  if(!is.null(inv)){
+    message("getting cached data")
+    return(inv)
+  }
+  data<-x$get()
+  inv<-solve(data)
+  x$setInverse(m)
+  inv
+}
